4958b5ee4206fbf8bf5634b1eee6aaaf2aee9af7
Josh Biedrzycki <nyzzik@gmail.com>
2025-03-10 12:01:58 -0400

    AstraScans: Fix Latest Updates selector

--- src/AstraScans/main.ts
+++ src/AstraScans/main.ts
@@ -1,3 +1,4 @@
+import { CheerioAPI } from "cheerio";
 import { MangaStreamGeneric } from "../generic/MangaStream";
 import config from "./pbconfig";
 
@@ -6,6 +7,11 @@ const DOMAIN_NAME: string = "https://astrascans.org";
 class AstraScansExt extends MangaStreamGeneric {
     domain = DOMAIN_NAME;
     name = config.name;
+
+    override configureSections(): void {
+        this.latestUpdatesSection.selectorFunc = ($: CheerioAPI) =>
+            $("div.bsx", $("h2:contains(Latest Update)")?.parent()?.next());
+    }
 }
 
 export const AstraScans = new AstraScansExt();
