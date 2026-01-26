1d24845675208b1d7f362d8bccd699e0c0ad8538
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-01-25 22:37:54 -0800

    chore(generic): version bump

--- src/SushiScans/pbconfig.ts
+++ src/SushiScans/pbconfig.ts
@@ -1,11 +1,12 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /* Copyright © 2026 Inkdex */
 
-import { basePbConfig } from "../generic/config";
+import { basePbConfig, customVersion } from "../generic/config";
 
 let pbConfig = basePbConfig;
 
 pbConfig.name = "SushiScans";
 pbConfig.description = "Extension that pulls content from sushiscan.net.";
+pbConfig.version = customVersion({ increasePrerelease: 1 });
 
 export default pbConfig;
