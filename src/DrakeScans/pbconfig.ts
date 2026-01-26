4efd5294edd7177b8bbe302ad38880a87013b624
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-01-25 22:37:54 -0800

    chore(generic): version bump

--- src/DrakeScans/pbconfig.ts
+++ src/DrakeScans/pbconfig.ts
@@ -1,11 +1,12 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /* Copyright © 2026 Inkdex */
 
-import { basePbConfig } from "../generic/config";
+import { basePbConfig, customVersion } from "../generic/config";
 
 let pbConfig = basePbConfig;
 
 pbConfig.name = "Drake Scans";
 pbConfig.description = "Extension that pulls content from drakecomic.org.";
+pbConfig.version = customVersion({ increasePrerelease: 1 });
 
 export default pbConfig;
